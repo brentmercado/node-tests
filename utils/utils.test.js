fab9ac7323c5dcf1a27e549b9c4ea8d08e832130
Brent Mercado <brent.mercado.566@my.csun.edu>
2018-02-01 09:45:32 -0800

    Assertion library with expect

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,44 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
    var res = utils.add(33, 11);
 
-   if (res !== 44) {
-      throw new Error(`Expected 44, but got ${res}.`);
-   }
+   expect(res).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
    var res = utils.square(5);
 
-   if (res !== 25) {
-      throw new Error(`Expected 25, but got ${res}.`);
-   }
-})
+   expect(res).toBe(25).toBeA('number');
+});
+
+// should verify first and last names are set
+// assert it includes firstName and lastName with proper values
+it('should set first and last names', () => {
+   var user = {
+      age: 28,
+      location: 'Los Angeles'
+   };
+   var res = utils.setName(user, 'Brent Mercado');
+
+   expect(res).toInclude({
+      firstName: 'Brent',
+      lastName: 'Mercado'
+   });
+});
+
+
+// it('should expect some values', () => {
+//    // expect(12).toNotBe(12);
+//    // expect({name: 'Brent'}).toNotEqual({name: 'Bren'});
+//    // expect([2,3,4]).toExclude(1);
+//    expect({
+//       name: 'Brent',
+//       age: 28,
+//       location: "LA"
+//    }).toExclude({
+//       age: 23
+//    });
+// });

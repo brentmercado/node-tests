368d938678546e35880195a5c5045ec287a889a4
Brent Mercado <brent.mercado.566@my.csun.edu>
2018-02-03 10:37:54 -0800

    Testing express apps using supertest

--- server/server.js
+++ server/server.js
@@ -3,7 +3,27 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-   res.send('Hello world!');
+   res.status(404).send({
+      error: 'Page not found.',
+      name: 'Todo App v1.0'
+   });
 });
 
+app.get('/users', (req, res) => {
+   res.send([{
+      name: 'Mike',
+      age: 27
+   }, {
+      name: 'Brent',
+      age: 28
+   }, {
+      name: 'Tim',
+      age: 26
+   }]);
+});
+// GET /users
+// Give users a name property and age property
+
 app.listen(3000);
+
+module.exports.app = app;

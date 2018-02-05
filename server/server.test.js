420a4ab60af8ef2ab97574ae3f58900a9fb0ce96
Brent Mercado <brent.mercado.566@my.csun.edu>
2018-02-04 18:37:36 -0800

    Organized tests into describe blocks

--- server/server.test.js
+++ server/server.test.js
@@ -3,30 +3,41 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return hello world response', (done) => {
-   request(app)
-      .get('/')
-      .expect(404)
-      .expect((res) => {
-         expect(res.body).toInclude({
-            error: 'Page not found.'
-         });
-      })
-      .end(done);
-});
+// server
+   // GET /
+      // some test case
+   // Get / users
+      // some test case
 
-// Make a new test
-// assert 200
-// assert that you exist in users array
-it('should return my user object', (done) => {
-   request(app)
-      .get('/users')
-      .expect(200)
-      .expect((res) => {
-         expect(res.body).toInclude({
-            name: 'Brent',
-            age: 28
-         });
-      })
-      .end(done);
+describe('Server',() => {
+
+   describe('GET /', () => {
+      it('should return hello world response', (done) => {
+         request(app)
+            .get('/')
+            .expect(404)
+            .expect((res) => {
+               expect(res.body).toInclude({
+                  error: 'Page not found.'
+               });
+            })
+            .end(done);
+      });
+   });
+
+   describe('GET /users', () => {
+      it('should return my user object', (done) => {
+         request(app)
+            .get('/users')
+            .expect(200)
+            .expect((res) => {
+               expect(res.body).toInclude({
+                  name: 'Brent',
+                  age: 28
+               });
+            })
+            .end(done);
+      });
+   });
+
 });
